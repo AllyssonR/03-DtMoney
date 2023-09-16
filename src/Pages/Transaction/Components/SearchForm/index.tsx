71d6777969354f3ece7aefbf3cefd4a1cfa041dc
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2023-09-15 21:43:35 -0300

    add react-hook-form and zod

--- src/Pages/Transaction/Components/SearchForm/index.tsx
+++ src/Pages/Transaction/Components/SearchForm/index.tsx
@@ -1,9 +1,26 @@
 import { MagnifyingGlass } from '@phosphor-icons/react'
 import { SearchFormContainer } from './styles'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+type SearchformInputs = z.infer<typeof searchFormSchema>
 export function SearchForm() {
+  const { register, handleSubmit } = useForm<SearchformInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+  function handleSearchTransactions(data: SearchformInputs) {
+    console.log(data)
+  }
   return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque uma transação" name="" id="" />{' '}
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque uma transação"
+        {...register('query')}
+      />{' '}
       <button type="submit">
         <MagnifyingGlass size={20} />
         Buscar
